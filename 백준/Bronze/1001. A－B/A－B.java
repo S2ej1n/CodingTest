a7699db1b2e2587a0d7c0cae2712451df15484b8
강세진 <110716496+S2ej1n@users.noreply.github.com>
2025-01-14 11:33:51 +0900

    [Bronze V] Title: A-B, Time: 100 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        String[] tlist = input.split(" ");
+
+        int A = Integer.parseInt(tlist[0]);
+        int B = Integer.parseInt(tlist[1]);
+        System.out.println(A-B);
+    }
+}
