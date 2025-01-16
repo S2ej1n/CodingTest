73ec5481d362b15e74519ede7244064444a043ef
강세진 <110716496+S2ej1n@users.noreply.github.com>
2025-01-16 16:25:54 +0900

    [Bronze IV] Title: 영수증, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,16 +1,19 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        int X = scanner.nextInt();
-        int N = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         int sum = 0;
 
         for (int i = 0; i < N; i++) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             sum += a * b;
         }
 
