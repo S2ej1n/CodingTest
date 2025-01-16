af9cf6c9e6f4619b41db02377ce30f8fa8ebabde
강세진 <110716496+S2ej1n@users.noreply.github.com>
2025-01-16 16:17:36 +0900

    [Bronze V] Title: 합, Time: 168 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -5,7 +5,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        int sum = 0;
 
-        System.out.println( n*(n+1)/2 );
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
     }
 }
