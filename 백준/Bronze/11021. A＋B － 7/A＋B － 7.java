6de5f87c9d07565cd59d58b6ba947c43b8d51bd9
강세진 <110716496+S2ej1n@users.noreply.github.com>
2025-01-16 16:48:10 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 16028 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,4 +1,8 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
