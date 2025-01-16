154cfddd2bf373c21e128d811066bac88bcef31c
강세진 <110716496+S2ej1n@users.noreply.github.com>
2025-01-16 17:00:23 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 140 ms, Memory: 16292 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String str ="";
+        for (int i = 0; i < N; i++) {
+            str += "*";
+            System.out.println(String.format("%"+N+"s",str));
+        }
+    }
+}
