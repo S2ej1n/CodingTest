d9990a5760c9691ed991683d9b54c299c8ab9ca1
강세진 <110716496+S2ej1n@users.noreply.github.com>
2025-01-23 15:50:07 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 108 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        outer:
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            boolean isGroup = true;
+
+            // 문자열만큼 반복
+            for (int j = 0; j < input.length(); j++) {
+                char ch = input.charAt(j);
+                String newInput = input.substring(j+1, input.length());
+
+                if (newInput.indexOf(ch) > 0) { //그룹이 아닌지 확인
+                    isGroup = false;
+                    continue outer;
+                }
+            }
+
+            if (isGroup) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
