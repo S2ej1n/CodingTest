041dfbf93da2f001bd526a90ab6f65423f08c930
강세진 <110716496+S2ej1n@users.noreply.github.com>
2025-01-23 14:24:37 +0900

    [Bronze I] Title: 단어 공부, Time: 276 ms, Memory: 26868 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -9,13 +9,11 @@ public class Main {
         char[] upWordArray = upWord.toCharArray();
 
         int c = 0;
-        // set으로 바꾸고, 그것만 확인하기
         Set<Character> set = new HashSet<>();
         for (Character word : upWordArray) {
             set.add(word);
         }
         Character[] set2Array = set.toArray(new Character[set.size()]);
-        // set 배열로 변경하기 - set은 순서가 없는 컬렉션이당용
 
         int[] counts = new int[set.size()];
         for ( int i = 0; i < set.size(); i++ ) {
@@ -29,7 +27,6 @@ public class Main {
         int max = Integer.MIN_VALUE;
         int index = 0;
         boolean doublemax = false;
-
         for ( int i = 0; i < counts.length; i++ ) {
             if ( counts[i] > max ) {
                 max = counts[i];
