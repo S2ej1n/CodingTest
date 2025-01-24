d3589e626a58b31982ab649f257e81118db6d20a
강세진 <110716496+S2ej1n@users.noreply.github.com>
2025-01-24 16:15:31 +0900

    [Bronze III] Title: 최댓값, Time: 72 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -12,14 +12,9 @@ public class Main {
         for (int i = 0; i < 9; i ++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             
-            outer:
             for (int j = 0; j < 9; j++) {
                 matrix[i][j] = Integer.parseInt(st.nextToken());
-                if(matrix[i][j] == 99){
-                    max = 99;
-                    index = (i+1) + " " + (j+1);
-                    break outer;
-                } else if(matrix[i][j] > max){
+                if(matrix[i][j] > max){
                     max = matrix[i][j];
                     index = (i+1) + " " + (j+1);
                 }
