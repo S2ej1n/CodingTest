02c0f4d7e08a01e131703b95a3522d5bd0f92f10
Sejin <110716496+S2ej1n@users.noreply.github.com>
2025-04-29 20:41:39 +0900

    [Silver III] Title: 소수 구하기, Time: 296 ms, Memory: 42212 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,12 +1,16 @@
 import math
 M, N = map(int, input().split())
 
-#나 or 1
-for i in range(M,N+1):
-    if i < 2:
-        continue # 밑에 안읽음
-    for j in range(2, int(math.sqrt(i))+1):
-        if i % j == 0:
-            break
-    else:
+is_prime = [True] * (N + 1)
+is_prime[0] = is_prime[1] = False #소수 아님 표시
+
+for i in range(2, int(math.sqrt(N)) + 1):
+    if is_prime[i]: # i가 소수일 때 
+        for j in range(i * i, N + 1, i): 
+            # ex. 2*5, 3*5, 4*5는 앞전 2,3에서 다 지웠으므로 5*5부터 제거한다.
+            # 5*5, 5*6은 5의 배수니까.!
+            is_prime[j] = False
+
+for i in range(M, N + 1):
+    if is_prime[i]:
         print(i)
