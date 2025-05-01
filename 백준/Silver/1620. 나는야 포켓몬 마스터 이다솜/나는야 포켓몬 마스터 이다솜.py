ed9e840dfb8a84e59fde283aac3a73708618491e
Sejin <110716496+S2ej1n@users.noreply.github.com>
2025-05-01 13:47:00 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 7820 ms, Memory: 54600 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,10 +1,10 @@
-# 도감에 수록된 포켓몬 개수 N, 내가 맞춰야하는 문제 개수 M
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
 dict={}
 dict2={}
+
 for i in range(N):
     poket = input()
     dict[i+1] = poket
@@ -12,8 +12,7 @@ for i in range(N):
 
 for j in range(M):
     q = input()
-    try:
-        q = int(q)
-        print(dict[q])
-    except ValueError : #str
+    if q.isdigit():
+        print(dict[int(q)])
+    else:
         print(dict2[q])
