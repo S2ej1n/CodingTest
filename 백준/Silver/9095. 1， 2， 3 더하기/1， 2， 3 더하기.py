f0714771ec0efe3a6b767a9e4ba1790ffabde493
Sejin <110716496+S2ej1n@users.noreply.github.com>
2025-08-08 19:48:59 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -5,9 +5,9 @@ def ans(n):
     if n <= 3:
        return dp[n]
     else:
+        dp = dp + [0] * (n-3)
         for i in range(4,n+1):
-            dp.append(dp[i-1]+dp[i-2]+dp[i-3])
-            # dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
         return dp[n]
 
 for i in range(T):
