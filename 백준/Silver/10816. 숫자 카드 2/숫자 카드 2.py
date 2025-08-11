09b928833297a350a02c6dd0fb8564eb4280a77f
Sejin <110716496+S2ej1n@users.noreply.github.com>
2025-08-11 18:06:13 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 2820 ms, Memory: 115796 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -7,8 +7,7 @@ N_list.sort()
 # 끝 인덱스 - 시작 인덱스 + 1 = 개수
 # 현재 아이디어는 start search 는 시작인덱스 저장하고, end search는 끝 인덱스 저장.
 # 시작 인덱스를 찾는 이진 탐색
-def start_search(arr, target):
-    start_idx = -1
+def binary_search(arr, target):
     start = 0
     end = N-1
 
@@ -16,41 +15,18 @@ def start_search(arr, target):
         mid = (start + end) // 2
 
         if arr[mid] == target:
-            start_idx = mid
-            
-            # 더 왼쪽에 같은 값이 있는지 탐색할것.
             end = mid - 1
+            return cnt.get(target) #찾았으면 dict에서 개수 반환
         elif arr[mid] > target:
             end = mid - 1
         elif arr[mid] < target:
             start = mid + 1
-    
-    return start_idx
+    return 0
 
-# 끝 인덱스를 찾는 이진 탐색
-def end_search(arr, target):
-    end_idx = -1
-    start = 0
-    end = N-1
-
-    while start <= end:
-        mid = (start + end) // 2
-
-        if arr[mid] == target:
-            end_idx = mid
-
-            # 더 오른쪽에 값이 있는지 탐색
-            start = mid + 1
-        elif arr[mid] < target:
-            start = mid + 1
-        else:
-            end = mid -1
-    return end_idx
-
+# 빈도 dict
+cnt = {}
+for v in N_list:
+    cnt[v] = cnt.get(v, 0) + 1
 
 for target in M_list:
-    if end_search(N_list, target) == -1:
-        print(0)
-    else:
-        reslt = end_search(N_list, target) - start_search(N_list, target) + 1
-        print(reslt)
+    print(binary_search(N_list, target))
