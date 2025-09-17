22d39445ea5b7d6003d4546ed96ab98287b69548
Sejin <110716496+S2ej1n@users.noreply.github.com>
2025-09-18 02:42:08 +0900

    [Gold V] Title: 토마토, Time: 2332 ms, Memory: 108320 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+M, N = map(int, input().split())
+tomato_box = [list(map(int, input().split())) for _ in range(N)]
+
+# 방문 여부
+visited = [[False] * M for _ in range(N)]
+
+# 상하좌우
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+queue = deque()
+day = 0
+
+for i in range(N):
+    for j in range(M):
+        if tomato_box[i][j] == 1:
+            queue.append((i, j, 0))
+            visited[i][j] = True
+
+while queue:
+    # 현재 좌표
+    cx, cy, cd= queue.popleft()
+    day = cd
+
+    for i in range(4):
+        nx = cx + dx[i]
+        ny = cy + dy[i]
+
+        if 0 <= nx < N and 0 <= ny < M:
+            if tomato_box[nx][ny] == 0 and visited[nx][ny] == False:
+                visited[nx][ny] = True
+                tomato_box[nx][ny] = 1
+                queue.append((nx, ny, cd+1))
+
+if any(0 in row for row in tomato_box):
+    print("-1")
+else:
+    print(day)
