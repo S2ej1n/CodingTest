e67237616d2cfe01e76cd55102cb18ba21bf7132
Sejin <110716496+S2ej1n@users.noreply.github.com>
2025-11-22 11:18:22 +0900

    [Gold V] Title: 평범한 배낭, Time: 2420 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,5 +1,3 @@
-# 물품의 수 N, 버틸 수 있는 무게 K
-
 N, K = map(int, input().split())
 weight = []
 value = []
@@ -9,17 +7,15 @@ for i in range(N):
     weight.append(W)
     value.append(V)
 
-# 2차원 이상 리스트 만들 땐 항상 [...] for _ in range(...)
-# dp[i][w] = (앞에서 i개의 물건만 고려했을 때, 배낭 용량이 w일 때 가질 수 있는)최대 가치
-dp = [[0]*(K+1) for _ in range(N+1)]
+dp = [0] * (K + 1)
 
-# 현재진행 N, 현재의 남아있는 무게 j. weight
-for i in range(1,N+1):
-    for j in range(K+1):    
-        if j < weight[i-1]:
-            dp[i][j] = dp[i-1][j]
-        elif j >= weight[i-1]:
-            dp[i][j] = max(dp[i-1][j], value[i-1] + dp[i-1][j-weight[i-1]])
+# i번째 아이템을 고려
+for i in range(N):
+    w = weight[i]
+    v = value[i]
 
+    # 무게 한도 K부터 w까지 역순으로
+    for cur in range(K, w - 1, -1):
+        dp[cur] = max(dp[cur], dp[cur - w] + v)
 
-print(dp[N][K])
+print(dp[K])
