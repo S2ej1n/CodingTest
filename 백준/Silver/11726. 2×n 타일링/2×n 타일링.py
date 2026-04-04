7fbd3abd1b13bc87d872f804b89006d72356e5ff
Sejin <110716496+S2ej1n@users.noreply.github.com>
2026-04-04 22:20:40 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+'''
+n = 1 1개
+n = 2 || = 2개
+n = 3 | ||   | =  =| 3개
+n = n-1 n-2
+'''
+ar = [0,1,2]
+
+for i in range(3, n+1):
+    ar.append(ar[i-2] + ar[i-1])
+
+print(ar[n]%10007)
