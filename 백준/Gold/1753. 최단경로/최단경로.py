0c2c7018c79684d60914431753ef667732bb40b3
Sejin <110716496+S2ej1n@users.noreply.github.com>
2026-04-04 22:38:01 +0900

    [Gold IV] Title: 최단경로, Time: 668 ms, Memory: 69516 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,44 +1,34 @@
-#1753 최단경로
 import sys
 import heapq
+input = sys.stdin.readline
 
-# 정점개수, 간선 개수
-V,E = map(int, sys.stdin.readline().split())
-# 시작점
-K = int(sys.stdin.readline())
+V, E = map(int, input().split())
+K = int(input()) # 시작점
 
-# 그래프만들기
-graph = {n: [] for n in range(1, V + 1)}
+INF = int(1e9)
+edge = [[] for _ in range(V+1)]
+dist = [INF] * (V+1)
 
-for i in range(E):
-    u, v, w = map(int, sys.stdin.readline().split())
-    graph[u].append((v, w))
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    edge[u].append((w, v)) # 가중치 먼저
 
-# 다익스트라
-def dstra(graph, K):
-    distance = {node: float('inf') for node in graph }
+dist[K] = 0
+heap = []
+heapq.heappush(heap, (0,K))
 
-    distance[K] = 0
-    heap = [(0,K)]
+while heap:
+    ew, ev = heapq.heappop(heap)
 
-    while heap:
-        dist, now = heapq.heappop(heap)
+    if ew != dist[ev] : continue
 
-        if dist > distance[now]:
-            continue
+    for nw, nv in edge[ev]:
+       if  dist[nv] > nw + dist[ev]:
+           dist[nv] = nw + dist[ev]
+           heapq.heappush(heap, (dist[nv],nv))
 
-        for neighbor, w in graph[now]:
-            cost = dist + w
-            if cost < distance[neighbor]:
-                distance[neighbor] = cost
-                heapq.heappush(heap, (cost, neighbor))
-    
-    return distance
-
-distance = dstra(graph,K)
-
-for i in range(1, V+1):
-    if distance[i] == float('inf'):
+for i in range(1,V+1):
+    if dist[i] == INF:
         print('INF')
     else:
-        print(distance[i])
+        print(dist[i])
